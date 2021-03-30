e6de32b8f258709547067d6841e9af2777d48206
lisswart <arnoudlisa@gmail.com>
2021-03-29 22:06:09 -0400

    commit tree 08c19f59d977cb6e3bf3762df1e843a74727cc9f

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31, num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 14, num4 = 10;
+const mod = num3 % num4;
